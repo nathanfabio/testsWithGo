a993dac0ec3fa140cc8af15cf8c82111084676e6
Nathan Fabio <nathan_3_m@hotmail.com>
2024-02-25 18:41:13 -0300

    hello world v2

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello()
-	expect := "Hello, World!"
+	result := Hello("Nathan")
+	expect := "Hello, Nathan"
 
 	if result != expect {
 		t.Errorf("result: %s, expect: %s", result, expect)
