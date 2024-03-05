3bff17a897cbde7ee6ce69890ba09e57b37d5a1d
Nathan Fabio <nathan_3_m@hotmail.com>
2024-03-05 06:37:13 -0300

    arrays and slices v3

--- arrays_slices/arraysSilices_test.go
+++ arrays_slices/arraysSilices_test.go
@@ -1,6 +1,9 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -14,5 +17,39 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", result, expected, numbers)
 			
 		}
+	})
+}
+
+// func TestSumAll(t *testing.T) {
+// 	result:= SumAll([]int{2, 3}, []int{3, 3})
+// 	expected:= []int{5, 6}
+
+// 	if !reflect.DeepEqual(result, expected) {
+// 		t.Errorf("got %v want %v", result, expected)
+// 	}
+// }
+
+func TestSumAllTheRest(t *testing.T) {
+	checkSums:= func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+
+	t.Run("sum up some slices", func(t *testing.T){
+		result:= SumAllTheRest([]int{1, 4}, []int{0, 7})
+		expected:= []int{4, 7}
+
+		checkSums(t, result, expected)
+	})
+
+
+	t.Run("sum up empty slices", func(t *testing.T){
+		result:= SumAllTheRest([]int{}, []int{3, 8, 2})
+		expected:= []int{0, 10}
+
+		checkSums(t, result, expected)
 	})
 }
--- arrays_slices/arraysSlices.go
+++ arrays_slices/arraysSlices.go
@@ -10,3 +10,28 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// func SumAll(numbersToSum ...[]int) [] int {
+// 	var sums []int
+
+// 	for _, numbers := range numbersToSum {
+// 		sums = append(sums, Sum(numbers))		
+// 	}
+
+// 	return sums
+// }
+
+func SumAllTheRest(numbersToSum ...[]int) [] int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			final:= numbers[1:]
+			sums = append(sums, Sum(final))
+		}
+	}
+
+	return sums
+}
