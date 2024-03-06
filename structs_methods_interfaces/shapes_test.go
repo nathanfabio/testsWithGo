71735b28a41210450f4938efec955a67c75d4a3b
Nathan Fabio <nathan_3_m@hotmail.com>
2024-03-06 19:18:59 -0300

    strucs, methods and interfaces v3

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -15,23 +15,22 @@ func TestPerimeter(t *testing.T) {
 
 
 func TestArea(t *testing.T) {
+	checkArea:= func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		result:= shape.Area()
+
+		if result != expected {
+			t.Errorf("result %.2f, expected %.2f", result, expected)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle:= Rectangle{20.0, 10.0}
-		result:= rectangle.Area()
-		expected:= 200.0
-
-		if result != expected {
-			t.Errorf("got %.2f want %.2f", result, expected)
-		}
+		checkArea(t, rectangle, 200.0)
 	})
 
-	t.Run("circles", func(t *testing.T)  {
-		circle:= Circle{5}
-		result:= circle.Area()
-		expected:= 78.53981633974483
-
-		if result != expected {
-			t.Errorf("got %.2f want %.2f", result, expected)
-		}
+	t.Run("circles", func(t *testing.T) {
+		circle:= Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
