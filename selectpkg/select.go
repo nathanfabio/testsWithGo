b062e8a1e57e49c59b740ed1b8e2b76ba171bafe
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-03-31 18:44:30 -0300

    select v3

--- selectpkg/select.go
+++ selectpkg/select.go
@@ -1,23 +1,28 @@
 package selectpkg
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Runner(a, b string) (winner string) {
-	timeA := responseTime(a)
-	timeB := responseTime(b)
-
-	if timeA < timeB {
-		return a
+func Runner(a, b string) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("time exceeded to %s and %s", a, b)
 	}
-
-	return b
 }
 
-func responseTime(URL string) time.Duration {
-	start := time.Now()
-	http.Get(URL)
-	return time.Since(start)
+func ping(URL string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(URL)
+		ch <- true
+	}()
+
+	return ch
 }
--- selectpkg/select_test.go
+++ selectpkg/select_test.go
@@ -8,6 +8,20 @@ import (
 )
 
 func TestRunner(t *testing.T) {
+	t.Run("return an error if server doesn't respond within 10s", func(t *testing.T) {
+		serverA := delayedServer(11 * time.Second)
+		serverB := delayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Runner(serverA.URL, serverB.URL)
+		if err == nil {
+			t.Errorf("error not found")
+		}
+	})
+
+
 	serverSlow := delayedServer(20 * time.Millisecond)
 	serverFast := delayedServer(0 * time.Millisecond)
 	
@@ -18,7 +32,7 @@ func TestRunner(t *testing.T) {
 	URLFast := serverFast.URL
 
 	expected := URLFast
-	result := Runner(URLSlow, URLFast)
+	result, _ := Runner(URLSlow, URLFast)
 
 	if result != expected {
 		t.Errorf("result '%s', expected '%s'", result, expected)
